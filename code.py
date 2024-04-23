84bc0ce94d998f83b8dcfe5b228638a4cb3f3a99
Mikael Amare <159163639+Mikael-Amare@users.noreply.github.com>
2024-04-23 11:10:43 -0400

    apple but better

--- code.py
+++ code.py
@@ -1,1 +1,31 @@
-print("Hello, World!")
+#!/usr/bin/env python3
+
+"""
+Created by: Mikael Amare
+Created on: Apr 2024
+This program is the "Space Aliens" program on the PyBadge
+"""
+
+import stage
+import ugame
+
+def game_scene() -> None:
+    """
+    This function is the main game game_scene
+    """
+
+    print("\n\n\n") # 3 blank lines
+
+
+    print("Hello, World!")
+
+
+    # repeat forever, game loop
+
+
+    while True:
+        pass # just a placeholder for now
+
+
+if __name__ == "__main__":
+    game_scene()
