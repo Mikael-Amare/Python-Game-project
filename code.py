dc8a8a337df948c9aa34da5feda623518a18e7be
Mikael Amare <159163639+Mikael-Amare@users.noreply.github.com>
2024-05-01 11:29:57 -0400

    close to my sunshine

--- code.py
+++ code.py
@@ -15,24 +15,25 @@ def game_scene() -> None:
     This function is the main game game_scene
     """
     # image banks for CircuitPython
-    image_bank_background stage.Bank.from_bmp16("my_sunshine_[MConverter.eu].bmp")
+    image_bank_background = stage.Bank.from_bmp16("my_sunshine_[MConverter.eu].bmp")
     # set the background to image 0 in the image bank
    
     # and the size (10x8 tiles of size 16x16)
     
-    background stage.Grid(image_bank_background, 10, 8)
+    background = stage.Grid(image_bank_background, 10, 8)
     # create a stage for the background to show up on
     # and set the frame rate to 60fps
 
-    game stage.Stage(ugame.display, 60)
+    game = stage.Stage(ugame.display, 60)
 
-    [background] game.layers
+    game.layers = [background]
     # set the layers of all sprites, items show up in order
     # render all sprites
-    # most likely you will only render the background once per game scene game.render_block()
+    # most likely you will only render the background once per game scene 
+    game.render_block()
     # repeat forever, game loop
     while True:
-    pass # just a placeholder for now
-    41 if __name__ == "__main__":
-    42 game_scene()
-    43
+        pass # just a placeholder for now
+if __name__ == "__main__":
+    game_scene()
+    
