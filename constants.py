e1bd5baf1a0874a832853b21a723387f27490f78
Mikael Amare <159163639+Mikael-Amare@users.noreply.github.com>
2024-05-14 11:22:31 -0400

    fixed py problem

--- constants.py
+++ constants.py
@@ -6,7 +6,7 @@ Created on: May 2024
 This constants file is for Space Alien game
 """
 
-# PyBadge screen size is 160x128 and sprites are 16 * 16
+# PyBadge screen size is 160x128 and sprites are 16x16
 SCREEN_X = 160
 SCREEN_Y = 128
 SCREEN_GRID_X = 10
@@ -14,7 +14,7 @@ SCREEN_GRID_Y = 8
 SPRITE_SIZE = 16
 TOTAL_NUMBER_OF_ALIENS = 5
 FPS = 60
-SPRITE_MOVEMENT_SPEED = 2
+SPRITE_MOVEMENT_SPEED = 1
 
 # Using for button state
 button_state = {
