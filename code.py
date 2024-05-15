92f5f60df60a86d7b10dc972f00b9d65409c0a93
Mikael Amare <159163639+Mikael-Amare@users.noreply.github.com>
2024-05-15 10:42:19 -0400

    lesson #7

--- code.py
+++ code.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 """
-Created by: Mikael Amare
+Created by: Jakub Malhotra
 Created on: May 2024
 This program is the "Space Aliens" program on the PyBadge
 """
@@ -9,6 +9,51 @@ This program is the "Space Aliens" program on the PyBadge
 import constants
 import stage
 import ugame
+
+def menu_scene():
+    """
+    This function is the menu scene
+    """
+
+    # image banks for CircuitPython
+    image_bank_mt_background = stage.Bank.from_bmp16("mt_game_studio.bmp")
+
+    # add text objects
+    text = []
+    text1 = stage.Text(width=29, height=12, font=None, palette=constants.RED_PALETTE, buffer=None)
+    text1.move(20, 10)
+    text1.text("MT Game Studios")
+    text.append(text1)
+
+    text2 = stage.Text(width=29, height=12, font=None, palette=constants.RED_PALETTE, buffer=None)
+    text2.move(40, 110)
+    text2.text("PRESS START")
+    text.append(text2)
+
+    # sets the background to image 0 in the image bank
+    background = stage.Grid(image_bank_mt_background, constants.SCREEN_X, constants.SCREEN_Y)
+
+    # create a stage for the background to show up on
+    # and set the frame rate to 60fps
+    game = stage.Stage(ugame.display, constants.FPS)
+
+    # set the layers, items show up in order
+    game.layers = text + [background]
+
+    # render the background and initial location of sprite list
+    game.render_block()
+
+    # repeat forever, game loop
+    while True:
+        # get user input
+        keys = ugame.buttons.get_pressed()
+
+        # Start button selected
+        if keys & ugame.K_START != 0:
+            game_scene()
+
+        # update game logic
+        game.tick()  # wait until refresh rate finishes
 
 def game_scene():
     """
@@ -32,13 +77,13 @@ def game_scene():
     sound.mute(False)
 
     # set the background to image 0 in the image bank
-    #   and the size (10x8 tiles of size 16x16)
+    # and the size (10x8 tiles of size 16x16)
     background = stage.Grid(image_bank_background, 10, 8)
 
     # a sprite that will be updated every frame
     ship = stage.Sprite(
         image_bank_sprites, 5, 75, constants.SCREEN_Y - (2 * constants.SPRITE_SIZE)
-   )
+    )
 
     alien = stage.Sprite(
         image_bank_sprites,
@@ -99,4 +144,4 @@ def game_scene():
         game.tick()  # wait until refresh rate finishes
 
 if __name__ == "__main__":
-    game_scene()
+    menu_scene()
